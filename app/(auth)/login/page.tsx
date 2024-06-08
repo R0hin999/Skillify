f6b8ea0b9635845497f6c51bfe672ec7eff347c2
Rohin <rohinbiyal@gmail.com>
2024-06-08 21:24:07 +0530

    final bug fixes

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,17 +5,18 @@ import { useEffect, useState } from "react";
 const LoginPage = () => {
   const [showAdminLogin, setShowAdminLogin] = useState(false);
   const handleTrigger = (event: KeyboardEvent) => {
-    if (event.key == "a"  && event.altKey) {
+    if (event.key == "a" && event.altKey) {
       setShowAdminLogin(!showAdminLogin);
     }
   };
   useEffect(() => {
     document.addEventListener("keydown", handleTrigger);
     return () => document.removeEventListener("keydown", handleTrigger);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [showAdminLogin]);
   return (
     <div className="h-screen w-screen flex items-center justify-center">
-      {showAdminLogin ? <AdminLoginForm/> : <LoginForm></LoginForm>}
+      {showAdminLogin ? <AdminLoginForm /> : <LoginForm></LoginForm>}
     </div>
   );
 };
